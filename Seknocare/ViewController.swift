ffeb2351dad08460ee045c7d49a17d3bc670e114
shengoo <shengoo@hotmail.com>
2017-02-14 23:24:58 +0800

    update status bar color

--- Seknocare/ViewController.swift
+++ Seknocare/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 import CoreBluetooth
 
 class ViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.setNeedsStatusBarAppearanceUpdate()
+    }
     
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
