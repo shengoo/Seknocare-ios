ef843c93285a4c7ad70f50695cd2506b6158aafb
shengoo <shengoo@hotmail.com>
2017-02-14 23:09:25 +0800

    init

---
+++ Seknocare/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Seknocare
+//
+//  Created by Qing Sheng on 2016/12/30.
+//  Copyright © 2016年 Qing Sheng. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ViewController: UIViewController{
+    
+
+
+}
+
