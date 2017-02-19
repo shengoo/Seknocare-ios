de7bd502be177e8c907ab334acc940200b1759dd
shengoo <shengoo@hotmail.com>
2017-02-19 22:32:16 +0800

    b

--- Seknocare/AppDelegate.swift
+++ Seknocare/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        CBCentralManager.init(delegate:  CBCentralManager.init(), queue: NSNull)
         return true
     }
 
--- Seknocare/ViewController.swift
+++ Seknocare/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController{
+class ViewController: UIViewController,
+    CBCentralManagerDelegate,
+CBPeripheralDelegate{
+    
+    var manager:CBCentralManager!
+    var discoverPeripherals = [CBPeripheral]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.setNeedsStatusBarAppearanceUpdate()
+        manager = CBCentralManager(delegate: self, queue: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            print("Bluetooth open.")
+            central.scanForPeripherals(withServices: nil, options: nil)
+            
+        } else {
+            print("Bluetooth not available.")
+                
+            // create the alert
+            let alert = UIAlertController(title: "Bluetooth", message: "Please turn on bluetooth.", preferredStyle: UIAlertControllerStyle.alert)
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+    }
+    
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if(peripheral.name == "BCM99"){
+            discoverPeripherals.append(peripheral)
+            print(peripheral)
+
+        }
+    }
+    
+    
+    
 
 }
 
